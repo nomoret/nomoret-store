c92bcf8bc0e756fc1f1ee7c164aed16be6a9a8df
br j <honuj9983@gmail.com>
2019-04-04 12:00:53 +0900

    Create Header Component  HOC

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import withLayout from "../lib/withLayout";
+
+const About = () => (
+  <div>
+    <Head>
+      <title>About | Nomoret Store</title>
+    </Head>
+    <h1>About page</h1>
+  </div>
+);
+
+export default withLayout(About);
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-export default () => <div>welcome to next js</div>;
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/Header";
+import withLayout from "../lib/withLayout";
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>Home | Nomoret Store</title>
+    </Head>
+    <h1>welcome to next js</h1>{" "}
+    <Link href={"/about"}>
+      <a>About page</a>
+    </Link>
+  </div>
+);
+
+export default withLayout(Index);
